d60a4fa46ed38c45e8d38c3d09717bdd2df93a61
mekesia-brown <mekesia.brown@mailchimp.com>
2021-07-10 14:46:29 -0400

    RPS exercise files

---
+++ index.js
@@ -0,0 +1,106 @@
+'use strict';
+
+let pcTally = [0, 0];
+let gameNum = 0;
+let randNum = null;
+let winningMove = "";
+let losingMove = "";
+let playerSelection = "";
+let computerSelection = "";
+let movesArray = ["rock", "paper", "scissors"];
+
+function playerPlay() {
+    pThrow = prompt("What is your move?");
+    pThrow = pThrow ? pThrow.toLowerCase() : null;
+
+    return pThrow;
+}
+
+function computerPlay() {
+    randNum = Math.floor(Math.random() * 3);
+
+    cThrow = movesArray[randNum];
+
+    return cThrow;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        switch(computerSelection) {
+            case "paper":
+                result = "lose";
+                winningMove = "paper";
+                losingMove = "beats rock";
+                pcTally[1] = pcTally[1]++;
+                break;
+            case "scissors":
+                result = "win";
+                winningMove = "rock";
+                losingMove = "beats scissors";
+                pcTally[0] = pcTally[0]++;
+                break;
+            default:  // computerSelection = "rock"
+                result = "are tied";
+                winningMove = "rock";
+                losingMove = "is the same as rock";
+        }
+    } else if (playerSelection === "paper") {
+        switch(computerSelection) {
+            case "scissors":
+                result = "lose";
+                winningMove = "scissors";
+                losingMove = "beats paper";
+                pcTally[1] = pcTally[1]++;
+                break;
+            case "rock":
+                result = "win";
+                winningMove = "paper";
+                losingMove = "beats rock";
+                pcTally[0] = pcTally[0]++;
+                break;
+            default: // computerSelection = "paper"
+                result = "are tied";
+                winningMove = "paper";
+                losingMove = "is the same as paper";
+        }
+    } else { // playerSelection === "scissors"
+        switch(computerSelection) {
+            case "paper":
+                result = "win";
+                winningMove = "scissors";
+                losingMove = "beats paper";
+                pcTally[0] = pcTally[0]++;
+                break;
+            case "rock":
+                result = "lose";
+                winningMove = "rock";
+                losingMove = "beats scissors";
+                pcTally[1] = pcTally[1]++;
+                break;
+            default: // computerSelection === "scissors"
+                result = "are tied";
+                winningMove = "scissors";
+                losingMove = "is the same as scissors";
+        }
+    }
+
+    winningMove = winningMove[0].toUpperCase() + winningMove.slice(1);
+
+    let resultStatement = `You ${result}! ${winningMove} ${losingMove}`;
+
+    return resultStatement;
+}
+
+function game() {
+    playerSelection = playerPlay();
+    computerSelection = computerPlay();
+
+    console.log(playRound(playerSelection, computerSelection));
+}
+
+while (gameNum < 5) {
+    game();
+    gameNum++;
+}
+
+console.log(`And the winner overall is ${pcTally[0] < pcTally[1] ? "You!" : "The computer"}`);
